6858b6ffcc45cc5d40fbc265b4b24798da4b0b8c
zastasja <anastasia_zh@bk.ru>
2023-01-19 15:49:01 +0300

    splitted java into classes

---
+++ py/java/nameChecker.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+//2. Составить алгоритм: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”,
+// если нет, то вывести "Нет такого имени"
+public class nameChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = sc.next();
+        String namecheck = "Вячеслав";
+        if (name.equals(namecheck)){
+            System.out.println("Привет, " + name);
+        } else {
+            System.out.println("Нет такого имени");
+        }
+        sc.close();
+    }
+}
