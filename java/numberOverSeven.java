b35161c97c0b1fb4050dedd78a18cd58956b5ee6
zastasja <anastasia_zh@bk.ru>
2023-01-19 17:15:04 +0300

    updated >7 method

--- java/numberOverSeven.java
+++ java/numberOverSeven.java
@@ -1,18 +1,23 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
  //1. Составить алгоритм: если введенное число больше 7, то вывести “Привет”.
 public class numberOverSeven {
 
     public static void main(String[] args) throws IOException {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите число: ");
+            double n = sc.nextDouble();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        double n = sc.nextDouble();
-
-        if (n > 7) {
-            System.out.println("Привет");
+            if (n > 7) {
+                System.out.println("Привет");
+            }
+            sc.close();
+        } catch (InputMismatchException e){
+            System.out.println("Неверный ввод");
         }
-    sc.close();
+
     }
 }
